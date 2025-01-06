58b5bdfb6ade784fa394508738ae75e6eb3288e6
Sacheth Nagamangala Srikanth <sachethns@gmail.com>
2025-01-06 01:32:01 -0500

    frontend

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAcVLAh9VXhGBjjHTSUEKB5ej4BIlptv3Y",
+  authDomain: "mern-estate-26462.firebaseapp.com",
+  projectId: "mern-estate-26462",
+  storageBucket: "mern-estate-26462.appspot.com",
+  messagingSenderId: "266657935969",
+  appId: "1:266657935969:web:dd985017a44da818c0f020"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./src/**/*.{html,js,jsx}", // Add jsx here
+];
+
+export const theme = {
+  extend: {},
+};
+
+export const plugins = [];

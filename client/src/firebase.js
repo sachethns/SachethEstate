d881bcbce35e840df5d0a2f82d34a3eee5a734a5
Sacheth Nagamangala Srikanth <sachethns@gmail.com>
2025-01-06 01:41:32 -0500

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAcVLAh9VXhGBjjHTSUEKB5ej4BIlptv3Y",
-  authDomain: "mern-estate-26462.firebaseapp.com",
-  projectId: "mern-estate-26462",
-  storageBucket: "mern-estate-26462.appspot.com",
-  messagingSenderId: "266657935969",
-  appId: "1:266657935969:web:dd985017a44da818c0f020"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY, // Use environment variable for API key
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN, // Use environment variable for auth domain
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID, // Use environment variable for project ID
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET, // Use environment variable for storage bucket
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID, // Use environment variable for messaging sender ID
+  appId: import.meta.env.VITE_FIREBASE_APP_ID, // Use environment variable for app ID
 };
 
 // Initialize Firebase
